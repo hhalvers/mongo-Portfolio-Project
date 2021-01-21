c454772374e5f703dc1d4d1cb5c47a2e1c033a8c
Heath Halverson <hhalver@me.com>
2021-01-21 12:28:06 -0800

    remove config.js part 2

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/realty',
-    'facebook': {
-        clientId: '424484905436759',
-        clientSecret: '7b048c61ddd561bbd22f6d41ae485e1a'
-    }
-}
