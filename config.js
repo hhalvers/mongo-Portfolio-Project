f405ca5dee937cb3e4747c3ff79d9d6055475a36
Heath Halverson <hhalver@me.com>
2021-01-26 14:23:08 -0800

    empty config checkinn

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'secretKey': '',
+    'mongoUrl': '',
+    'facebook': {
+        clientId: '',
+        clientSecret: ''
+    },
+    'GOOGLE_CLIENT_ID': '',
+    'GOOGLE_CLIENT_SECRET': '',
+    'SESSION_SECRET': ''
+}
